7457c74e42677081dd012af8d1fbb7d47452d882
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-03-31 09:02:24 +0300

    10

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 // import 'simplelightbox/src/simple-lightbox.scss';
 // import 'simplelightbox/dist/simple-lightbox.css';
 
