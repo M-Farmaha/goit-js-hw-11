5d263fb54e4653c71379e93055c433b7931ff619
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-03-31 14:28:04 +0300

    clear all

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import 'simplelightbox/src/simple-lightbox.scss';
-// import 'simplelightbox/dist/simple-lightbox.css';
 
 var lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
