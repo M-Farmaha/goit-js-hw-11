5ea37e093ae5e4718fb771a8943fec5e7e2d723f
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-03-31 15:41:58 +0300

    hope

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
 var lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
